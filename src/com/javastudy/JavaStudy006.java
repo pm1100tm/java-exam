a0ad8faaba52130d2662bb08357b08b958552688
dev-swd <tladnjsen@gmail.com>
2021-03-16 22:40:20 +0900

    [2021-03-16:SWD] 자바의 정석 10번 돌리기 1 - collection 1

--- src/com/javastudy/JavaStudy006.java
+++ src/com/javastudy/JavaStudy006.java
@@ -3,5 +3,77 @@ package com.javastudy;
 public class JavaStudy006 {
     public static void main(String[] args) {
         /* Collection - 1 */
+        
+        /* 컬랙션 프레임웍
+        * 여러 객체를 모아 놓은 것을 의미
+        * 핵심 인터페이스
+        * List, Set, Map
+        * 
+        * List - ArrayList, LinkedList, Stack, Vector
+        * 순서가 있는 데이터의 집합, 데이터의 중복을 허용
+        * 
+        * Set - HashSet, TreeSet
+        * 순서를 유지하지 않는 데이터의 집합. 데이터의 중복을 허용하지 않는다.
+        * 
+        * Map - HashMap, TreeMap, HashTable, Properties
+        * 키, 값 쌍으로 이루어진 데이터의 집합
+        * 순서는 유지되지 않으며, 키는 중복을 허용하지 않고, 값은 중복을 허용한다.
+        * 
+        * List 인터페이스의 메서드
+        * void add(int index, Object element)
+        * boolean addAll(int index, Collection c)
+        * Object get(int index)
+        * int indexOf(Object o)
+        * int lastIndexOf(Object o)
+        * ListIterator listIterator()
+        * ListIterator listIterator(int index)
+        * Object remove(int index, Object element)
+        * Object set(int index, Object element)
+        * void sort(Comparator c)
+        * List subList(int fromIndex, int toIndex)
+        * 
+        * Set 인터페이스
+        * boolean add(Object o)
+        * void clear()
+        * boolean contains(Object o)
+        * boolean equals(Object o)
+        * int hashCode()
+        * boolean isEmpty()
+        * iterator iterator()
+        * boolean remove(Object o)
+        * int size()
+        * Object[] toArray()
+        * Object[] toArray(Object[] a)
+        * 
+        * boolean addAll(Collection c) 합집합
+        * boolean containsAll(Collection c) 부분집한
+        * boolean removeAll(Collection c) 차집합
+        * boolean retainAll(Collection c) 교집합
+        * 
+        * Map 인터페이스의 메서드
+        * void clear()
+        * boolean containsKey(Object key)
+        * boolean containsValue(Object value)
+        * Set entrySet()
+        * boolean equals(Object o)
+        * Object get(Object key)
+        * int hashCode()
+        * boolean isEmpty()
+        * Set keySet()
+        * Object put(Object key, Object value)
+        * void putAll(Map t)
+        * Object remove(Object key)
+        * int size()
+        * Collection values()
+        * 
+        * Vector 와 ArrayList
+        * ArrayList는 기존의 Vector 를 개선한 것으로 구현원리와 기능적으로 동일
+        * Vector 는 자체적으로 동기화 처리가 되어있으나 ArrayList 는 그렇지 않다.
+        * 
+        * List 인터페이스를 구현하므로, 저장순서가 유지되고 중복을 허용한다.
+        * 데이터의 저장공간으로 배열을 사용한다. (배열 기반)
+        * **/
+        
+        
     }
 }
